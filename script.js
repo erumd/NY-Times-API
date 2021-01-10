66b37008d5f7494ae955390a21c1e1a1eaab2fbe
Erum Dhukka <erumdhukka531@gmail.com>
2021-01-10 14:01:01 -0600

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Linked to html
 // Always read getting started 
 
-$('#searchBtn').on('click',function () {
+
 
 
 var searchTerm = "";
@@ -21,61 +21,6 @@ var endYear= 0;
 var articleCounter= 0;
 
 
-// $(document).ready(function () {
-        
-
-        // runQuery (10, queryUrl);
-        console.log (searchBtn, "button works"); 
-
-
-      // get element value with jquery
-      searchTerm = $("#searchTerm").val();
-      $("#searchTerm").val(""); //not sure what this means
-    // add in search term 
-        console.log ("You searched for", searchTerm);
-
-        // ***** LEARNING POINT: HAVE TO CALL THE WEBSITE WITHIN FUNCTION TO REGISTER *********
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + APIKey;
-
-
-    console.log (queryURL, "This link with searchTerm results ");
-
-    // get number of records
-    numResults = $('#numberRecords').val();
-
-    //get the start and end year
-    startYear = $('#startYear').val().trim(); //format has to be month and year 
-    endYear = $('#endYear').val().trim(); //format has to be month and year 
-
-    if (parseInt(startYear)) {
-        startYear = startYear + "0101";
-        queryURL = queryURL + "&begin_date=" + startYear;
-    }
-    if (parseInt(endYear)) {
-        endYear = endYear +"0101";
-        queryURL = queryURL + "&end_date=" + endYear; 
-    }
-
-    // var newURL= 
-    // queryURL = queryURL + "&begin_date=" + startYear + "&end_date=" + endYear;
-    console.log (queryURL, "link article with with dates");
-
-
-    // articleSearch(searchTerm);
-
-
-    // articleSearch(searchTerm); // if you do NOT add searchTerm here then var searchTerm is not used. will NOT print in console. 
-    // Need to take the searchTerm and apply
-    // WHEN i comment out line 52 then ajax line line 71 works
-
-    runQuery (numResults, queryURL);
-
-    return false; 
-
-
-   
-   
-});
 
     function runQuery (numArticle, queryURL) {
         // var searchTerm = "";
@@ -134,22 +79,59 @@ var articleCounter= 0;
             $('#articleWell-' + i).append("<a href=" + NYData.response.docs[i].web_url + ">" + NYData.response.docs[i].web_url + "<a>");
 
         }
-
-     
         })
 
         
         console.log(queryUrl);
         console.log(numArticle);
         console.log (NYData, "link with search term");
-
-           
         // })
         // console.log("This is NY article search: ", response);
-
         // articleSearch (searchTerm);
         
     };
+
+    $('#searchBtn').on('click',function () {
+// $(document).ready(function () {
+        
+        // runQuery (10, queryUrl);
+        console.log (searchBtn, "button works"); 
+
+      // get element value with jquery
+      searchTerm = $("#searchTerm").val();
+      $("#searchTerm").val(""); //not sure what this means
+    // add in search term 
+        console.log ("You searched for", searchTerm);
+
+        // ***** LEARNING POINT: HAVE TO CALL THE WEBSITE WITHIN FUNCTION TO REGISTER *********
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + APIKey;
+
+    console.log (queryURL, "This link with searchTerm results ");
+
+    // get number of records
+    numResults = $('#numberRecords').val();
+
+    //get the start and end year
+    startYear = $('#startYear').val().trim(); //format has to be month and year 
+    endYear = $('#endYear').val().trim(); //format has to be month and year 
+
+    if (parseInt(startYear)) {
+        startYear = startYear + "0101";
+        queryURL = queryURL + "&begin_date=" + startYear;
+    }
+    if (parseInt(endYear)) {
+        endYear = endYear +"0101";
+        queryURL = queryURL + "&end_date=" + endYear; 
+    }
+
+    // var newURL= 
+    // queryURL = queryURL + "&begin_date=" + startYear + "&end_date=" + endYear;
+    console.log (queryURL, "link article with with dates");
+
+    runQuery (numResults, queryURL); //******************************************ERROR */
+
+    return false; 
+});
 
     
 
